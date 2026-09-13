c31ecb22c3d82ae8f9f9e3428726eb0e73ea59dc
Luka Simić <git@kocka.tech>
2026-09-13 08:32:29 +0200

    Post header customization by crushpunchh.

--- Themes/DUMBDefault/Display.template.php
+++ Themes/DUMBDefault/Display.template.php
@@ -675,7 +675,6 @@ function template_single_post($message)
 								</div>';
 
 	echo '
-								', !empty($message['counter']) ? '<span class="page_number floatright">#' . $message['counter'] . '</span>' : '', '
 								<div class="postinfo">
 									<span class="messageicon" ', ($message['icon_url'] === $settings['images_url'] . '/post/xx.png' && !$message['can_modify']) ? ' style="position: absolute; z-index: -1;"' : '', '>
 										<img src="', $message['icon_url'] . '" alt=""', $message['can_modify'] ? ' id="msg_icon_' . $message['id'] . '"' : '', '>
