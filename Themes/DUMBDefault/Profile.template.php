b7881b5d3727edf8194ac8c654a7f438d0a2cd16
Luka Simić <git@kocka.tech>
2026-09-18 15:27:02 +0200

    Remove clunky autocomplete workaround.

--- Themes/DUMBDefault/Profile.template.php
+++ Themes/DUMBDefault/Profile.template.php
@@ -1456,10 +1456,6 @@ function template_edit_options()
 
 	echo '
 		<form action="', $url, '" method="post" accept-charset="', $context['character_set'], '" name="creator" id="creator" enctype="multipart/form-data"', ($context['menu_item_selected'] == 'account' ? ' autocomplete="off"' : ''), '>
-			<div style="height:0;overflow:hidden;">
-				<input type="text" id="autocompleteFakeName">
-				<input type="password" id="autocompleteFakePassword">
-			</div>
 			<div class="cat_bar">
 				<h3 class="catbg profile_hd">';
 
